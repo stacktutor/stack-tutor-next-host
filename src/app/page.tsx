7ba18bc9cd201c41f1f3a3001c815acc7662617e
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-04-04 01:43:34 +0530

    add custom sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import SideBar from "@/components/SideBar";
+
 export default function Page() {
   return (
     <main className="grid grid-cols-12 gap-4 w-full px-4 py-6">
       {/* Sidebar */}
-      <aside className="col-span-3 hidden md:block bg-gray-100 dark:bg-gray-900 p-4 rounded-lg">
-        <h2 className="text-lg font-bold">Sidebar</h2>
-        <p>Navigation or extra content</p>
-      </aside>
+      <SideBar/>
 
       {/* Main Content */}
       <section className="col-span-12 md:col-span-6 bg-white dark:bg-black p-4 rounded-lg shadow">
@@ -14,7 +13,7 @@ export default function Page() {
       </section>
 
       {/* Ad Section */}
-      <aside className="col-span-3 hidden md:block bg-gray-100 dark:bg-gray-900 p-4 rounded-lg">
+      <aside className="col-span-2 hidden lg:block bg-gray-100 dark:bg-gray-900 p-4 rounded-lg">
         <h2 className="text-lg font-bold">Ads</h2>
         <p>Sponsored content</p>
       </aside>
